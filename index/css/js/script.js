73321cb0bb53c0419eaed178f5797422fb7451ef
Simo0501 <sim.manna1977@gmail.com>
2024-04-26 12:29:17 +0200

    sistemato html

---
+++ index/css/js/script.js
@@ -0,0 +1,13 @@
+// Crea 4 input:
+// 1. Nome utente: deve contenere almeno 4 caratteri e non più di 10.
+// 2. Password: deve essere lunga almeno 8 caratteri e non più di 10.
+// 3. Ripeti Password: deve essere identica alla password inserita precedentemente.
+// 4. Checkbox Privacy: deve essere selezionata per procedere.
+// E un bottone
+// Implementa i seguenti controlli:
+// Verifica che il nome utente rispetti la lunghezza richiesta.
+// Assicurati che la password soddisfi i criteri di lunghezza.
+// Conferma che la seconda password corrisponda esattamente alla prima.
+// Controlla che la checkbox della privacy sia selezionata.
+// Per ogni errore inserisci un messaggio sul rispettivo input
+// Se tutti i controlli sono superati, mostra il messaggio: “Registrazione avvenuta con successo”. (modificato) 
